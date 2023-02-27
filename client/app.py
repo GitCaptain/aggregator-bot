b97777749f6ef564be9225354097424487a7ae63
Alexander Sukhikh <alexsm.rf24@gmail.com>
2023-02-28 02:46:08 +0300

    Add file parsing functional, add join channel funcitonal

--- client/app.py
+++ client/app.py
@@ -1,17 +1,24 @@
 import logging
 from telethon import TelegramClient
 from bot import Bot
-
+from file_processor import FileProcessor
 class App:
 
-    def __init__(self, api_id: str, api_hash: str, channel_file: str, session_name: str) -> None:
+    def __init__(self,
+                 api_id: str,
+                 api_hash: str,
+                 main_channel: str,
+                 channel_file: str,
+                 session_name: str) -> None:
         self.api_id = api_id
         self.api_hash = api_hash
-        self.channel_file = channel_file
         self.session_name = session_name
         self.logger = logging.getLogger('Main.app')
+        self.main_channel = main_channel
+        self.file_processor = FileProcessor(channel_file)
 
     def start(self) -> None:
         self.logger.info('App started')
         with TelegramClient(self.session_name, self.api_id, self.api_hash) as client:
-            client.loop.run_until_complete(Bot(client).start())
+            client.loop.run_until_complete(Bot(client, self.main_channel, self.file_processor)
+                                            .start())
